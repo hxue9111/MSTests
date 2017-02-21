08ac58b085556df6df29413e54c2efe6b38af42b
vinh nguyen <vnguyen1415@gmail.com>
2017-02-21 02:28:29 -0500

    Changing timer, calculate score on both left hand and right hand

--- app/src/main/java/cmsc436/mstests/LevelTestActivity.java
+++ app/src/main/java/cmsc436/mstests/LevelTestActivity.java
@@ -18,7 +18,7 @@ import android.widget.TextView;
 
 
 @TargetApi(Build.VERSION_CODES.CUPCAKE)
-public class LevelTestActivity extends Activity implements SensorEventListener {
+public class LevelTestActivity extends Activity {
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
     float x, y, z;
@@ -26,11 +26,12 @@ public class LevelTestActivity extends Activity implements SensorEventListener {
     Button level_test_button ;
     TextView text_prompt;
     ImageView img;
-    String left_hand_test_label = "Press the button to hold your phone on left hand";
-    String right_hand_test_label = "Press the button to hold your phone on right hand";
+    String left_hand_test_label = "Hold your phone on left hand as steady as possible ";
+    String right_hand_test_label = "Hold your phone on right hand as steady as possible ";
     String start_label = "Push to start test";
-    double score;
+    double score, left_hand_score = -1 , right_hand_score = -1;
     private LevelView visual;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +39,8 @@ public class LevelTestActivity extends Activity implements SensorEventListener {
 
         visual = (LevelView)findViewById(R.id.visual);
         visual.setVisibility(View.INVISIBLE);
+
+
 
         level_test_button = (Button)findViewById(R.id.level_test_start);
         text_prompt = (TextView)findViewById(R.id.level_test_prompt);
@@ -50,6 +53,9 @@ public class LevelTestActivity extends Activity implements SensorEventListener {
         text_prompt.setText(hand_test_name);
         level_test_button.setVisibility(View.VISIBLE);
         level_test_button.setText(start_label);
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(listener, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         level_test_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,81 +65,56 @@ public class LevelTestActivity extends Activity implements SensorEventListener {
 
     }
     //Start countdown
-    private void setCountdownView() {
+    private double setCountdownView() {
+        score = 0;
         level_test_button.setVisibility(View.GONE);
-        new CountDownTimer(4000, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-                text_prompt.setText("Test starting in: " + millisUntilFinished / 1000);
-            }
-
-            public void onFinish() {
-                levelTestStart();
-            }
-        }.start();
-    }
-
-    public void resultVIew() {
-        text_prompt.setText("Your score is : " + Math.round(score));
-    }
-
-    @TargetApi(Build.VERSION_CODES.CUPCAKE)
-    public void levelTestStart() {
         visual.setVisibility(View.VISIBLE);
-        level_test_button.setVisibility(View.INVISIBLE);
-//        ImageView img = (ImageView)findViewById(R.id.target);
-//        img.setVisibility(View.VISIBLE);
-
-
-        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
         new CountDownTimer(10000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                text_prompt.setText("seconds remaining: " + millisUntilFinished / 1000);
+                text_prompt.setText("Second Remaining " + millisUntilFinished / 1000);
             }
 
-            @Override
             public void onFinish() {
                 visual.setVisibility(View.INVISIBLE);
-                text_prompt.setVisibility(View.INVISIBLE);
-                resultVIew();
-                unregistered(senSensorManager);
+
+                if (left_hand_score == -1) {
+                    left_hand_score  = score;
+                    senSensorManager.unregisterListener(listener);
+                    setStartView(right_hand_test_label);
+                } else if (right_hand_score == -1){
+                    right_hand_score = score ;
+                    senSensorManager.unregisterListener(listener);
+                    resultView();
+                }
+
             }
-
-        };
+        }.start();
+        return score;
     }
 
-    public void unregistered(SensorManager sensor) {
-        sensor.unregisterListener(this);
+    //set result
+    public void resultView() {
+       // text_prompt.setText("Your score is : " + Math.round(score));
+        text_prompt.setText("Results:\n Left hand: " + Math.round(left_hand_score) +
+                "\nRight hand: " + Math.round(right_hand_score));
     }
 
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-    }
-    /*
-    public void displayCurrentValues() {
 
-        currentX = (TextView)findViewById(R.id.display_x);
-        currentY = (TextView)findViewById(R.id.display_y);
-        currentZ = (TextView)findViewById(R.id.display_z);
-
-        currentX.setText("Current X : " + Float.toString(x));
-        currentY.setText("Current Y : " + Float.toString(y));
-        currentZ.setText("Current Z : " + Float.toString(z));
-
-    }*/
+    private final SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
 
 
-    @TargetApi(Build.VERSION_CODES.CUPCAKE)
-    public void onSensorChanged(SensorEvent event) {
-        Sensor mySensor = event.sensor;
+        @TargetApi(Build.VERSION_CODES.CUPCAKE)
+        public void onSensorChanged(SensorEvent event) {
+            Sensor mySensor = event.sensor;
 
-        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            x = event.values[0];
-            y = event.values[1];
-            z = event.values[2];
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                x = event.values[0];
+                y = event.values[1];
+                z = event.values[2];
 
             /*currentX = (TextView)findViewById(R.id.display_x);
             currentY = (TextView)findViewById(R.id.display_y);
@@ -143,25 +124,25 @@ public class LevelTestActivity extends Activity implements SensorEventListener {
             currentY.setText("Current Y : " + Float.toString(y));
             currentZ.setText("Current Z : " + Float.toString(z));
 */
-            if(score(x,y,z)){
-                visual.updateDrawing(x,y,z);
+                if (score(x, y, z)) {
+                    visual.updateDrawing(x, y, z);
+                }
+
+            }
+        }
+
+        public boolean score(float x, float y, float z) {
+            final int SENSITIVITY = 1;
+            double displacement = Math.abs(x) + Math.abs(y);
+
+            if (displacement > SENSITIVITY) {
+                score += displacement;
+                return true;
             }
 
+            return false;
         }
-    }
-
-    public boolean score (float x, float y, float z) {
-
-        final int SENSITIVITY = 1;
-        double displacement = Math.abs(x) + Math.abs(y) ;
-
-        if (displacement > SENSITIVITY) {
-            score += displacement;
-            return true;
-        }
-
-        return false;
-    }
+        };
 }
 
 
