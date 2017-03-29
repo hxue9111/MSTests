04543a2bc74124f395a67537ebd5cf1dce84ba42
katelin232 <katelin232@gmail.com>
2017-03-29 18:57:39 -0400

    Bicep Leg Test files

---
+++ BicepLegTestActivity.java
@@ -0,0 +1,205 @@
+package cmsc436.mstests;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class BicepLegTestActivity extends Activity {
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    TextView tv;
+    Button start_button;
+    Button done_button;
+    String right_arm = "Start Right Arm Bicep Curls";
+    String left_arm = "Start Left Arm Bicep Curls";
+    String right_leg = "Start Right Leg Curls";
+    String left_leg = "Start Left Leg Curls";
+
+    ArrayList<Long> right_arm_list = new ArrayList<Long>();
+    ArrayList<Long> left_arm_list = new ArrayList<Long>();
+    ArrayList<Long> right_leg_list = new ArrayList<Long>();
+    ArrayList<Long> left_leg_list = new ArrayList<Long>();
+
+
+    double ra_avg;
+    double la_avg;
+    double rl_avg;
+    double ll_avg;
+
+    BicepCurlSensorEventListener evl;
+
+    String currLimb = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bicep_leg_test);
+        tv = (TextView) findViewById(R.id.text_view);
+        start_button = (Button) findViewById(R.id.start_btn);
+        done_button = (Button) findViewById(R.id.done_btn);
+        done_button.setVisibility(View.GONE);
+        //tv.setText("0 Curls");
+        System.out.println("activity started");
+        setRightArmView(right_arm);
+        //init();
+    }
+
+    public void init(){
+        evl = new BicepCurlSensorEventListener(new CurlCallback(tv));
+        evl.init();
+        start_button.setVisibility(View.GONE);
+        done_button.setVisibility(View.VISIBLE);
+        done_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                System.out.println("done clicked");
+                System.out.println("currLimb" + currLimb);
+                if(currLimb.equals("right_arm")){
+                    ra_avg = average(evl.curlTimes)/1000;
+                    setLeftArmView(left_arm);
+                }else if(currLimb.equals("left_arm")){
+                    la_avg = average(evl.curlTimes)/1000;
+                    setRightLegView(right_leg);
+                }else if(currLimb.equals("right_leg")){
+                    rl_avg = average(evl.curlTimes)/1000;
+                    setLeftLegView(left_leg);
+                }else if(currLimb.equals("left_leg")){
+                    ll_avg = average(evl.curlTimes)/1000;
+                    setResultsView();
+                }
+                evl.curlTimes.clear();
+            }
+        });
+        tv.setText("0 Curls");
+        //evl.curlTimes.size(); // number reps
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        senSensorManager.registerListener(evl, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public class CurlCallback{
+        TextView tv;
+        public CurlCallback(TextView tv){
+            this.tv = tv;
+        }
+
+        public void onCurl(ArrayList<Long> curlTimes){
+            int rep = curlTimes.size();
+            tv.setText(rep + " Curls");
+
+//            if(currLimb.equals("right_arm")){
+//                right_arm_list = curlTimes;
+//            }else if(currLimb.equals("left_arm")){
+//                left_arm_list = curlTimes;
+//            }else if(currLimb.equals("right_leg")){
+//                right_leg_list = curlTimes;
+//            }else if(currLimb.equals("left_leg")){
+//                left_leg_list = curlTimes;
+//            }
+            //once rep = 10 can move to the next task, get array and store it, compute the averages and such
+            if(rep == 10){
+                if(currLimb.equals("right_arm")){
+                    ra_avg = average(evl.curlTimes)/1000.0;
+                    setLeftArmView(left_arm);
+                }else if(currLimb.equals("left_arm")){
+                    la_avg = average(evl.curlTimes)/1000.0;
+                    setRightLegView(right_leg);
+                }else if(currLimb.equals("right_leg")){
+                    rl_avg = average(evl.curlTimes)/1000.0;
+                    setLeftLegView(left_leg);
+                }else if(currLimb.equals("left_leg")){
+                    ll_avg = average(evl.curlTimes)/1000.0;
+                    setResultsView();
+                }
+                curlTimes.clear();
+            }
+        }
+    }
+
+    // Make the text take up the whole screen
+    private void setRightArmView(String right_arm) {
+        tv.setText(right_arm);
+        start_button.setVisibility(View.VISIBLE);
+        done_button.setVisibility(View.GONE);
+        currLimb = "right_arm";
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                init();
+            }
+        });
+    }
+
+    private void setLeftArmView(String left_arm) {
+        tv.setText(left_arm);
+        start_button.setVisibility(View.VISIBLE);
+        done_button.setVisibility(View.GONE);
+        currLimb = "left_arm";
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                init();
+            }
+        });
+    }
+
+    private void setRightLegView(String right_leg) {
+        tv.setText(right_leg);
+        start_button.setVisibility(View.VISIBLE);
+        done_button.setVisibility(View.GONE);
+        currLimb = "right_leg";
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                init();
+            }
+        });
+    }
+
+    private void setLeftLegView(String left_leg) {
+        tv.setText(left_leg);
+        start_button.setVisibility(View.VISIBLE);
+        done_button.setVisibility(View.GONE);
+        currLimb = "left_leg";
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                init();
+            }
+        });
+    }
+    private void setResultsView(){
+        start_button.setVisibility(View.GONE);
+        done_button.setVisibility(View.GONE);
+        tv.setText("Average Time Per Curl Results\n\n" +
+                "Right Arm: " + ra_avg + " seconds" +
+                "\nLeft Arm: " + la_avg + " seconds" +
+                "\nRight Leg: " + rl_avg + " seconds" +
+                "\nLeft Leg: " + ll_avg + " seconds");
+    }
+
+    private double average(ArrayList<Long> arr){
+        double l = 0;
+        if(arr.size() == 0){
+            return l;
+        }else{
+            double sum = 0;
+            for(int i = 0; i < arr.size(); i++){
+                sum += arr.get(i);
+            }
+            l = sum/arr.size();
+            return l;
+        }
+
+    }
+
+}
---
+++ MainActivity.java
@@ -0,0 +1,87 @@
+package cmsc436.mstests;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+        Button tap_test_button = (Button)findViewById(R.id.tap_test_start);
+        Button spiral_test_button = (Button)findViewById(R.id.spiral_test_start);
+        Button level_test_button = (Button)findViewById(R.id.level_test_start);
+        Button bicep_leg_test_button = (Button)findViewById(R.id.bicep_leg_test_start);
+
+        final Intent start_tap_test = new Intent(this, TapTestActivity.class);
+        final Intent start_spiral_test = new Intent(this, SpiralTestActivity.class);
+        final Intent start_level_test = new Intent(this, LevelTestActivity.class);
+        final Intent start_bicep_leg_test = new Intent(this, BicepLegTestActivity.class);
+
+        tap_test_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(start_tap_test);
+            }
+        });
+
+        spiral_test_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(start_spiral_test);
+            }
+        });
+
+        level_test_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(start_level_test);
+            }
+        });
+
+        bicep_leg_test_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(start_bicep_leg_test);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
