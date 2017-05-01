4bc81df5772a6e59f689991991eb64dad75bd786
gottemhus <gottemhus@users.noreply.github.com>
2017-05-01 16:50:17 -0400

    Add files via upload

--- app/src/main/java/cmsc436/mstests/SymbolTestActivity.java
+++ app/src/main/java/cmsc436/mstests/SymbolTestActivity.java
@@ -1,15 +1,24 @@
 package cmsc436.mstests;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.speech.RecognizerIntent;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewDebug;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,6 +28,7 @@ import java.util.Random;
 public class SymbolTestActivity extends Activity {
     double start_time,end_time;
     Button start_button, done_button ;
+    Button speech_recog;
     TextView prompt, result;
     NumberPicker np;
     ImageView symbol ;
@@ -31,6 +41,8 @@ public class SymbolTestActivity extends Activity {
     double[] trials = new double[9];
     String[] trials_check = new String[9];
 
+    protected static final int RESULT_SPEECH = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,62 +52,42 @@ public class SymbolTestActivity extends Activity {
         numbers = new ArrayList<Integer>();
         for (int i = 1; i < 10; ++i) {numbers.add(i); }
         Collections.shuffle(numbers);
-
+        speech_recog = (Button) findViewById(R.id.speech);
         start_button = (Button) findViewById(R.id.symbol_test_start);
-        done_button = (Button) findViewById(R.id.done_button);
         prompt = (TextView) findViewById(R.id.symbol_prompt);
         symbol = (ImageView) findViewById(R.id.symbol);
         symbol_list = (ImageView) findViewById(R.id.symbol_list);
         symbol_list.setImageResource(R.drawable.symbol_list);
         result = (TextView) findViewById(R.id.result);
+        input = (EditText) findViewById(R.id.answer);
 
         prompt.setText("Please take a look at the chart above then enter the correct number that corresponds to the image appear below and press Done! after you select");
-        np = (NumberPicker) findViewById(R.id.numberPicker);
-        np.setMaxValue(9);
-        np.setMinValue(0);
-        np.setWrapSelectorWheel(false);
-
-//        String str = "";
-//        if( numbers.isEmpty()) {
-//            prompt.setText("EMPTY");
-//        }else {
-//
-//            for (int i : numbers) {
-//                str = str + (String.valueOf(i));
-//            }
-//
-//            prompt.setText(str);
-//        }
 
         start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 prompt.setVisibility(View.INVISIBLE);
+
                 init();
             }
         });
 
     }
     public void init() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,1);
+
         start_button.setVisibility(View.INVISIBLE);
         if(count < 9) {
             doTest();
         }
         else {
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
             np.setVisibility(View.INVISIBLE);
-            done_button.setVisibility(View.INVISIBLE);
+            input.setVisibility(View.INVISIBLE);
+            speech_recog.setVisibility(View.INVISIBLE);
             resultView();
         }
-//        symbol.setImageResource(R.drawable.num1);
-//        n = np.getValue();
-//        prompt.setText(String.valueOf(n));
-//        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-//            @Override
-//            public void onValueChange(NumberPicker picker, int oldVal, int newVal){
-//                //Display the newly selected number from picker
-//                prompt.setText("Selected Number : " + newVal);
-//            }
-//        });
     }
     public void resultView() {
         String str = "";
@@ -107,28 +99,79 @@ public class SymbolTestActivity extends Activity {
     }
     public void doTest() {
 
-        getSymbol(numbers.get(count));
-        start_time = System.currentTimeMillis();
-        done_button.setOnClickListener(new View.OnClickListener(){
+        speech_recog.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                double final_time = 0;
-                if (np.getValue() == numbers.get(count)) {
-                    end_time = System.currentTimeMillis();
-                    final_time = ( end_time -start_time)/1000;
-                    trials[count] = final_time;
-                    trials_check[count] = "Correct";
-                }
-                else {
-                    trials[count] = final_time;
-                    trials_check[count] = "Wrong";
-                }
-                count++;
-                init();
 
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+                try{
+                    startActivityForResult(intent, RESULT_SPEECH);
+                }catch (ActivityNotFoundException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SPEECH NOT SUPPORTED",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+        getSymbol(numbers.get(count));
+        start_time = System.currentTimeMillis();
+            input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                    if (actionId == EditorInfo.IME_ACTION_DONE) {
+                        double final_time = 0;
+                        if( input.getText().toString().equals("")) {
+                            trials_check[count] = "Wrong";
+                        }
+                        else {
+                            if (Integer.parseInt(input.getText().toString()) == numbers.get(count)) {
+                                trials_check[count] = "Correct";
+                            } else {
+
+                                trials_check[count] = "Wrong";
+                            }
+                        }
+                        end_time = System.currentTimeMillis();
+                        final_time = (end_time - start_time) / 1000;
+                        trials[count] = final_time;
+
+                        count++;
+                        input.setText("");
+                        init();
+
+                        return true;
+                    } else {
+
+                        return false;
+                    }
+                }
+            });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+                    switch (data.getStringExtra(RecognizerIntent.EXTRA_RESULTS)) {
+                        case "one" :
+
+                    }
+                    ArrayList<String> text = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    result.setText(text.get(0));
+                }
+                break;
+            }
+
+        }
     }
 
     public void getSymbol(int n) {
